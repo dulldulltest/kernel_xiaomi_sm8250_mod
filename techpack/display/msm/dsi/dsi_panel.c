918852a76e0e48cee70074ad07272cdb5ca6d71c
YangQi0408 <a123er3643@163.com>
2025-03-15 17:21:26 +0800

    增添 elish 对第三方 REC 的支持 （例如：https://github.com/ymdzq/OFRP-device_xiaomi_elish/tree/fox_12.1-a14）

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -1146,6 +1146,19 @@ error:
 	return rc;
 }
 
+#if IS_ENABLED(CONFIG_SUPPORT_ELISH_REC)
+
+static bool in_recovery;
+
+static int __init boot_mode_setup(char *value)
+{
+	in_recovery = !strcmp(value, "1");
+	return 1;
+}
+__setup("androidboot.force_normal_boot=", boot_mode_setup);
+
+#endif
+
 static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 				  struct dsi_parser_utils *utils)
 {
@@ -1180,6 +1193,14 @@ static int dsi_panel_parse_timing(struct dsi_mode_info *mode,
 	rc = utils->read_u32(utils->data,
 				"qcom,mdss-dsi-panel-framerate",
 				&mode->refresh_rate);
+
+#if IS_ENABLED(CONFIG_SUPPORT_ELISH_REC)
+
+        if(!in_recovery)
+		mode->refresh_rate = 104;
+
+#endif
+
 	if (rc) {
 		DSI_ERR("failed to read qcom,mdss-dsi-panel-framerate, rc=%d\n",
 		       rc);
@@ -1792,6 +1813,15 @@ static int dsi_panel_parse_dfps_caps(struct dsi_panel *panel)
 	const char *name = panel->name;
 	const char *type;
 	u32 i;
+
+#if IS_ENABLED(CONFIG_SUPPORT_ELISH_REC)
+
+    if(!in_recovery) {
+		dfps_caps->dfps_support = false;
+		return rc;
+	}
+
+#endif
 
 	supported = utils->read_bool(utils->data,
 			"qcom,mdss-dsi-pan-enable-dynamic-fps");
