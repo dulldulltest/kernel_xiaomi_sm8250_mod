533984f450092a992d88d187be22773c3d6fd26a
dulldulltest <dummydulldulltest@gmail.com>
2025-11-17 21:17:50 +0800

    Revert "zram_drv: Hardcode disksize to 12GB"
    This reverts commit 30fc82c203b23b5f5d81e1d823b9471ec4c9fa0e.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2451,7 +2451,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = (u64)12288 * SZ_1M;
+	disksize = memparse(buf, NULL);
 	if (!disksize)
 		return -EINVAL;
 
