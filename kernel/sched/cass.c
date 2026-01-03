94d5fb7dfc4c78eac90e35c90f441021cdbd025e
dulldulltest <dummydulldulltest@gmail.com>
2026-01-03 14:24:09 +0800

    Revert "sched/cass: Only treat sync waker CPU as idle if there's one task running"
    This reverts commit a0b1b1f81d29087ee827d0d189702b7c99684fc4.

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -145,10 +145,9 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 
 		/*
 		 * Check if this CPU is idle or only has SCHED_IDLE tasks. For
-		 * sync wakes, treat the current CPU as idle if @current is the
-		 * only running task.
+		 * sync wakes, always treat the current CPU as idle.
 		 */
-		if ((sync && cpu == this_cpu && rq->nr_running == 1) ||
+		if ((sync && cpu == this_cpu) ||
 		    available_idle_cpu(cpu) || sched_idle_cpu(cpu)) {
 			/* Discard any previous non-idle candidate */
 			if (!has_idle)
